a4a3dd56320d0b05da36d4673f9e411b68bfe09e
Noah Albertsen <albertsennk@g.cofc.edu>
2019-04-15 19:47:04 -0400

    working on database functions

--- app/src/main/java/com/example/android/agameofgames/MainActivity.java
+++ app/src/main/java/com/example/android/agameofgames/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.android.agameofgames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 1;
+
+    public static final String QUIZ_ID = "com.example.android.agameofgames.ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String id = "bread";
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), QuizPage.class);
+                i.putExtra(QUIZ_ID, "bread");
+                startActivity(i);
+            }
+        });
+
+//        // choosing authentication providers
+//        List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                new AuthUI.IdpConfig.EmailBuilder().build(),
+//                new AuthUI.IdpConfig.GoogleBuilder().build()
+//        );
+//
+//        // creating and launching sign-in intent
+//        startActivityForResult(
+//                AuthUI.getInstance()
+//                        .createSignInIntentBuilder()
+//                        .setAvailableProviders(providers)
+//                        .build(),
+//                RC_SIGN_IN);
+
+
     }
+
+
+    // adding authentication thing
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RC_SIGN_IN) {
+//            IdpResponse response = IdpResponse.fromResultIntent(data);
+//
+//            if (resultCode == RESULT_OK) {
+//                // successfully signed in
+//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//            } else {
+//                // sign in failed
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/example/android/agameofgames/QuizPage.java
+++ app/src/main/java/com/example/android/agameofgames/QuizPage.java
@@ -1,13 +1,67 @@
 package com.example.android.agameofgames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class QuizPage extends AppCompatActivity {
 
+//    Quiz page;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_page);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra(MainActivity.QUIZ_ID);
+
+//        Quiz page;
+
+        // initializing views
+        final TextView question = findViewById(R.id.question);
+        final Button op1 = findViewById(R.id.op1);
+        final Button op2 = findViewById(R.id.op2);
+        final Button op3 = findViewById(R.id.op3);
+        final Button op4 = findViewById(R.id.op4);
+        Button next = findViewById(R.id.next);
+
+        // setting button click listeners
+
+
+        // initializing Cloud Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+//        Quiz quiz = new Quiz("Pick a color:", "red", "yellow", "blue", "green");
+//        db.collection("quizzes").document("color").set(quiz);
+
+        DocumentReference docRef = db.collection("quizzes")
+                .document(id).collection("questions")
+                .document("question1");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Quiz quiz = documentSnapshot.toObject(Quiz.class);
+
+                question.setText(quiz.getQuestion());
+                op1.setText(quiz.getOp1());
+                op2.setText(quiz.getOp2());
+                op3.setText(quiz.getOp3());
+                op4.setText(quiz.getOp4());
+            }
+        });
+
+
+
+
+
     }
 }
